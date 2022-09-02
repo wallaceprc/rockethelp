3eabf9901142ff3e30868bb753ccd022af5834af
Wallace Rocha <wallacerochascom@gmail.com>
2022-09-02 16:55:00 -0300

    first commit

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+import { Input as NativeBaseInput, IInputProps } from 'native-base';
+
+export function Input({...rest}: IInputProps) {
+  return (
+    <NativeBaseInput 
+      bg="gray.700"
+      h={14}
+      size="md"
+      fontFamily="body"
+      color="white"
+      placeholderTextColor="gray.300"
+      {...rest}
+    />
+  );
+}
